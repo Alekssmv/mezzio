87534d4af38fe4309e81c0476e1a74dec6821134
Александр Смирнов <alsmirnov@team.amocrm.com>
2023-12-16 15:44:21 +0300

    master

---
+++ src/App/Handler/SumHandler.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Handler;
+
+use Laminas\Diactoros\Response\JsonResponse;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class SumHandler implements RequestHandlerInterface
+{
+    public function handle(ServerRequestInterface $request) : ResponseInterface
+    {
+        $a = (int) $request->getAttribute('a');
+        $b = (int) $request->getAttribute('b');
+        $sum = $a + $b;
+        return new JsonResponse(['sum' => $sum]);
+    }
+}
