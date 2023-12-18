93b1d356864514c87346353f8799a5c4b0029416
Александр Смирнов <alsmirnov@team.amocrm.com>
2023-12-18 10:38:02 +0300

    Перенес Dotenv в index

--- config/container.php
+++ config/container.php
@@ -3,14 +3,9 @@
 declare(strict_types=1);
 
 use Laminas\ServiceManager\ServiceManager;
-use Dotenv\Dotenv;
 
 // Load configuration
 $config = require __DIR__ . '/config.php';
-
-// Load environment variables
-$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
 
 $dependencies                       = $config['dependencies'];
 $dependencies['services']['config'] = $config;
