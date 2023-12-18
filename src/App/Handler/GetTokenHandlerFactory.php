77d68960e900583f85409a6663525d9f99eca8c5
Александр Смирнов <alsmirnov@team.amocrm.com>
2023-12-18 18:41:14 +0300

    Убрал лишние хэндлеры

--- src/App/Handler/GetTokenHandlerFactory.php
+++ src/App/Handler/GetTokenHandlerFactory.php
@@ -4,17 +4,21 @@ declare(strict_types=1);
 
 namespace App\Handler;
 
+use AmoCRM\Client\AmoCRMApiClient;
 use Psr\Container\ContainerInterface;
 use App\Handler\GetTokenHandler;
-use App\Client\ApiClient;
-use App\Client\HttpClient;
+use GuzzleHttp\Client;
 
 class GetTokenHandlerFactory
 {
     public function __invoke(ContainerInterface $container) : GetTokenHandler
     { 
-        $apiClient = $container->get(ApiClient::class);
-        $httpClient = $container->get(HttpClient::class);
+        $apiClient = new AmoCRMApiClient(
+            $_ENV["AMO_CLIENT_ID"],
+            $_ENV["AMO_CLIENT_SECRET"],
+            $_ENV["AMO_REDIRECT_URI"],
+        );
+        $httpClient = new Client();
         
         return new GetTokenHandler($apiClient, $httpClient);
     }
