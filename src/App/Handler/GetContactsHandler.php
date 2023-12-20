a291973a94288a52d8a9106f79a4ed51b6ee4727
Александр Смирнов <alsmirnov@team.amocrm.com>
2023-12-20 21:45:19 +0300

    Добавл phpdoc

--- src/App/Handler/GetContactsHandler.php
+++ src/App/Handler/GetContactsHandler.php
@@ -13,37 +13,47 @@ use AmoCRM\Client\AmoCRMApiClient;
 use App\Helper\TokenActions;
 use Exception;
 
+/*
+ * Маршрут для получения списка контактов
+ * Принимает url параметр account_id
+ * Возвращает ошибку, если account_id не передан
+ * Возвращает ошибку, если токен не найден
+ * Возвращает ошибку, если токен истек
+ * Возвращает ошибку, если нет контактов
+ * Возвращает список контактов в формате JSON
+ */
 class GetContactsHandler implements RequestHandlerInterface
 {
     public function __construct(private AmoCRMApiClient $apiClient
     ) {
     }
-    
+
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         $params = $request->getQueryParams();
         $apiClient = $this->apiClient;
-        
+
         try {
             if (!isset($params['account_id'])) {
                 return new JsonResponse(['error' => 'account_id is required']);
             }
 
             $accessToken = TokenActions::getToken($params['account_id']);
-
             if ($accessToken === null) {
                 return new JsonResponse(['error' => 'token not found']);
-            }  
+            }
+            if ($accessToken->hasExpired()) {
+                return new JsonResponse(['error' => 'token expired']);
+            }
+
             $baseDomain = $accessToken->getValues()['baseDomain'];
             $apiClient->setAccessToken($accessToken)->setAccountBaseDomain($baseDomain);
-            dd($apiClient->contacts()->get());
-            
+            $contacts = $apiClient->contacts()->get();
 
         } catch (Exception $e) {
             return new JsonResponse(['error' => $e->getMessage()]);
         }
 
-
-        return new JsonResponse(['success' => 'ok']);
+        return new JsonResponse($contacts);
     }
 }
