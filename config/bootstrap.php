af8bb01fa963a575e16cabc0664cfe1c94faecfa
Александр Смирнов Викторович <alsmirnov@team.amocrm.com>
2024-01-15 16:01:12 +0100

    Убрал комментарий из bootstrap

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -15,18 +15,3 @@ $capsule = new Capsule();
 $capsule->addConnection($config['database']);
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
-
-// /**
-//  * Создаем очередь
-//  */
-// $queue = Pheanstalk::create('103.106.2.148', 11300);
-
-// /**
-//  * Кладем в очередь задачу на вывод текущего времени
-//  */
-// $queue->useTube('times')->put(json_encode('now'));
-
-// /**
-//  * Кладем в очередь задачу на вывод текущего времени на момент создания задачи
-//  */
-// $queue->useTube('times')->put(json_encode(time()));
